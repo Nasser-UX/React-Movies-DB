3fc44438f65c4192c13f977dc0d24d78bf1fb528
Ahmed Nasser <nasserart2017@gmail.com>
2021-08-22 21:03:29 +0200

    upload

---
+++ src/components/Home.js
@@ -0,0 +1,52 @@
+import React from 'react';
+//config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+//components
+import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner' 
+import SearchBar from './SearchBar' 
+
+//hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
+
+//image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+
+    const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
+    console.log(state);
+    return(
+        <>
+            {!searchTerm && state.results[0] ? ( 
+            <HeroImage
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview} 
+            /> 
+            ) : null
+            }
+
+            <SearchBar setSearchTerm={setSearchTerm} />
+
+            <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+                {state.results.map(movie => (
+                   <Thumb key={movie.id} 
+                        clickable={true}
+                        image={
+                            movie.poster_path
+                            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                            : NoImage
+                        } 
+                        movieId={movie.id}
+                   />
+                ))}
+            </Grid>
+            <Spinner />
+        </>
+    ) 
+}
+
+export default Home;
